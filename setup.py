b9af22b8cffae7b2d4e5301f1f64898f4f14d244
Deepak Hazarika <deepchin@users.noreply.github.com>
2020-09-07 10:10:29 +0530

    v1

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-tfdeepnet", # Replace with your own username
+    name="mygitpackage-tfdeepnet", # Replace with your own username
     version="0.0.1",
     author="Deepak",
     author_email="author@example.com",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/tfdeepnet/mygitpackage",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
